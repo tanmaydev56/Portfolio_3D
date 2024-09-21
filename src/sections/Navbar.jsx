1103d8fa324bf5f4ac7dfeed0fa7629f935bd074
tanmaydev56 <tanmaysharma763@gmail.com>
2024-09-21 19:21:17 +0530

    new1

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -27,8 +27,12 @@ const Navbar = () => {
     <header className='fixed top-0 left-0 right-0 z-50 bg-black/90'>
         <div className='max-w-[80rem] mx-auto'>
             <div className='flex justify-between items-center py-5 mx-auto  sm:px-5 px-10'>
-                <a href="/" className='text-neutral-400 font-bold text-xl hover:text-white transition-colors'>
+              <div className=''>
+              <a href="/" className='text-neutral-400 font-bold text-xl hover:text-white transition-colors flex gap-3 mt-1'>
+              <img src="/logo.svg" alt="" className='w-7 h-7 mt-0' />
+               
                 Tanmay</a>
+                </div>
                 <button className= 'sm:hidden flex text-neutral-400 hover:text-white  focus:outline-none   ' aria-label='Toggle menu' onClick={togglemenu}>
                 <img src={isopen ? "/assets/close.svg":"/assets/menu.svg"} alt="menu" className='w-6 h-6  
                 
